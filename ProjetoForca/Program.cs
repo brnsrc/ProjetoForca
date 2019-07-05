da90732db2d97c1892c73739fc10695c76eb521f
brnsrc <brunosrc@hotmail.com>
2019-07-05 00:19:10 -0300

    Projeto inicial de forca, iniciando lógica de interação

---
+++ ProjetoForca/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetoForca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letraBuscar = "a";
+            string palavraEscolhida;
+            string comparaPalavra = "lock";
+            Random random = new Random();
+            List<string> conjuntoDePalavras = new List<string>
+            {
+                "podre", "claridade", "céu", "vida", "mar", "rio de janeiro", "sao paulo",
+                "curitiba"
+            };   
+            //randomiza a palavra
+            palavraEscolhida = conjuntoDePalavras[random.Next(conjuntoDePalavras.Count)];
+            
+            Console.WriteLine($"letra: {letraBuscar}");
+
+            Console.WriteLine($"verifica Letra: {palavraEscolhida.Contains(letraBuscar)}");
+
+            Console.WriteLine(palavraEscolhida);
+            //Console.WriteLine(comparaPalavra == palavraEscolhida);
+
+
+            
+        }
+
+        public void AdicionarMembroDoCorpo(int numeroDoErro)
+        {
+            switch (numeroDoErro)
+            {
+                case 1:
+                    Console.WriteLine("        O");                
+                    break;
+                case 2:
+                    Console.Write("       /|");
+                    break;
+                case 3:
+                    Console.WriteLine("|\\  ");
+                    break;
+                case 4:
+                    Console.WriteLine("        |");
+                    break;
+                case 5:
+                    Console.Write("       / ");
+                    break;
+                case 6:
+                    Console.WriteLine("\\  ");
+                    break;
+            }
+        }
+    }
+}
